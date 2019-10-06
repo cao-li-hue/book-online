7449a6b3aa8ff66fac3f3268856261bb04c93a7c
caoli <1525180909@qq.com>
2019-10-06 14:35:02 +0800

    fist commit

---
+++ book-online/src/com/bo/entity/servlet/UserPageServlet.java
@@ -0,0 +1,23 @@
+package com.bo.entity.servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author li_cao
+ * @ClassName UserPageServlet
+ * @Description TODO
+ * @Date 2019/10/6
+ * @Version 1.0
+ **/
+@WebServlet(urlPatterns = "/user")
+public class UserPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("user.jsp").forward(req, resp);
+    }
+}
+
